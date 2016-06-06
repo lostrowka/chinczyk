43b4815baaad41e355f7375f7c132a8fee0be5ce
Łukasz Ostrówka <lostrowka@gmail.com>
2016-06-06 12:29:58 +0200

    Socket lokalnie

--- Main.js
+++ Main.js
@@ -1,66 +1,66 @@
 ﻿/// <reference path="libs/three.js" />
 
 function Main() {
-    var scene = new THREE.Scene();
-    var meshModel;
-    var camera = new THREE.PerspectiveCamera(
-        90, // kąt patrzenia kamery (FOV - field of view)
-        4 / 3, // proporcje widoku
-        0.1, // min renderowana odległość
-        10000 // max renderowana odległość
-    );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xFFFFFF);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+		var scene = new THREE.Scene();
+		var meshModel;
+		var camera = new THREE.PerspectiveCamera(
+				90, // kąt patrzenia kamery (FOV - field of view)
+				4 / 3, // proporcje widoku
+				0.1, // min renderowana odległość
+				10000 // max renderowana odległość
+		);
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor(0xFFFFFF);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
 
-    var axis = new THREE.AxisHelper(2000);
-    scene.add(axis);
+		var axis = new THREE.AxisHelper(2000);
+		scene.add(axis);
 
-    camera.position.x = 0;
-    camera.position.y = 400;
-    camera.position.z = 400;
-    scene.add(camera);
+		camera.position.x = 0;
+		camera.position.y = 400;
+		camera.position.z = 400;
+		scene.add(camera);
 
-    camera.lookAt(scene.position);
+		camera.lookAt(scene.position);
 
-    var client = io();
+		var client = io();
 
-    document.getElementById("start").onclick = function() {
-        document.getElementById("cover").style.visibility = "hidden";
-        controls = new THREE.OrbitControls( camera );
-        controls.addEventListener( 'change', renderer );
-        meshModel = model.returnModel();
-        meshModel.parseAnimations();
+		document.getElementById("start").onclick = function() {
+				document.getElementById("cover").style.visibility = "hidden";
+				controls = new THREE.OrbitControls( camera );
+				controls.addEventListener( 'change', renderer );
+				meshModel = model.returnModel();
+				meshModel.parseAnimations();
 
-        tablica = [];
-        for (var key in meshModel.geometry.animations) {
-            if (key === 'length' || !meshModel.geometry.animations.hasOwnProperty(key)) continue;
-            tablica.push(key);
-        }
-        meshModel.playAnimation(tablica[0], 5);
-        scene.add(meshModel);
-    }
+				tablica = [];
+				for (var key in meshModel.geometry.animations) {
+						if (key === 'length' || !meshModel.geometry.animations.hasOwnProperty(key)) continue;
+						tablica.push(key);
+				}
+				meshModel.playAnimation(tablica[0], 5);
+				scene.add(meshModel);
+		}
 
-    client.on("onconnect", function (data) {
-       alert(data.clientName)
-    })
+		client.on("onconnect", function (data) {
+			 alert(data.clientName)
+		})
 
-    var fieldElement = new FieldElement();
-    scene.add(fieldElement.getFieldElement());
+		var fieldElement = new FieldElement();
+		scene.add(fieldElement.getFieldElement());
 
-    var model = new Model();
+		var model = new Model();
 
-    var clock = new THREE.Clock();
+		var clock = new THREE.Clock();
 
-    function animateScene() {
-        requestAnimationFrame(animateScene);
-        renderer.render(scene, camera);
+		function animateScene() {
+				requestAnimationFrame(animateScene);
+				renderer.render(scene, camera);
 
-        //animacja modelu
-        var delta = clock.getDelta();
-        if (typeof meshModel != 'undefined')
-            meshModel.updateAnimation(delta * 1000);
-    }
-    animateScene();
+				//animacja modelu
+				var delta = clock.getDelta();
+				if (typeof meshModel != 'undefined')
+						meshModel.updateAnimation(delta * 1000);
+		}
+		animateScene();
 }
--- Server.js
+++ Server.js
@@ -4,105 +4,112 @@ var fs = require("fs")
 var socketio = require("socket.io")
 
 var server = http.createServer(function (req, res) {
-    console.log(req.method)
+		console.log(req.method)
 
-    switch (req.method) {
-        case "GET":
+		switch (req.method) {
+				case "GET":
 
-            console.log("Zadany: " + req.url)
+						console.log("Zadany: " + req.url)
 
-            if (req.url == "/") {
-                fs.readFile("index.html", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/style.css") {
-                fs.readFile("style.css", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/css' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/index.html") {
-                fs.readFile("index.html", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/Main.js") {
-                fs.readFile("Main.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/field/FieldElement.js") {
-                fs.readFile("field/FieldElement.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/field/Model.js") {
-                fs.readFile("field/Model.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/libs/OrbitControls.js") {
-                fs.readFile("libs/OrbitControls.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/libs/three.js") {
-                fs.readFile("libs/three.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/libs/tris.js") {
-                fs.readFile("libs/tris.js", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            else if (req.url == "/gfx/ralph.png") {
-                fs.readFile("gfx/ralph.png", function (error, data) {
-                    res.writeHead(200, { 'Content-Type': 'image/gif' });
-                    res.write(data);
-                    res.end();
-                });
-            }
-            break;
+						if (req.url == "/") {
+								fs.readFile("index.html", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'text/html' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/style.css") {
+								fs.readFile("style.css", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'text/css' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/index.html") {
+								fs.readFile("index.html", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'text/html' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/Main.js") {
+								fs.readFile("Main.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/field/FieldElement.js") {
+								fs.readFile("field/FieldElement.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/field/Model.js") {
+								fs.readFile("field/Model.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/libs/OrbitControls.js") {
+								fs.readFile("libs/OrbitControls.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/libs/three.js") {
+								fs.readFile("libs/three.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/libs/tris.js") {
+								fs.readFile("libs/tris.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/libs/socketio.js") {
+								fs.readFile("libs/socketio.js", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'application/javascript' });
+										res.write(data);
+										res.end();
+								});
+						}
+						else if (req.url == "/gfx/ralph.png") {
+								fs.readFile("gfx/ralph.png", function (error, data) {
+										res.writeHead(200, { 'Content-Type': 'image/gif' });
+										res.write(data);
+										res.end();
+								});
+						}
+						break;
 
-        case "POST":
-            servResp(req, res)
-            break;
-    }
+				case "POST":
+						servResp(req, res)
+						break;
+		}
 })
 
 function servResp(req, res) {
-    var allData = "";
+		var allData = "";
 
-    req.on("data", function (data) {
-        console.log("data: " + data)
-        allData += data;
-    })
+		req.on("data", function (data) {
+				console.log("data: " + data)
+				allData += data;
+		})
 
-    req.on("end", function (data) {
-        var finish = qs.parse(allData)
-        switch (finish.action) {
+		req.on("end", function (data) {
+				var finish = qs.parse(allData)
+				switch (finish.action) {
 
-        }
-    })
+				}
+		})
 }
 
 server.listen(3000);
@@ -111,9 +118,9 @@ console.log("serwer staruje na porcie 3000 - ten komunikat zobaczysz tylko raz")
 
 var io = socketio.listen(server)
 io.sockets.on("connection", function (client) {
-    console.log("klient sie podłączył" + client.id)
-    // client.id - unikalna nazwa klienta generowana przez socket.io
-    client.emit("onconnect", {
-        clientName: client.id
-    })
+		console.log("klient sie podłączył" + client.id)
+		// client.id - unikalna nazwa klienta generowana przez socket.io
+		client.emit("onconnect", {
+				clientName: client.id
+		})
 })
